bf413ec1b257e0755648a02051f69a611a7061f3
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-05-26 21:31:20 +0200

    null

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,5 +1,19 @@
 <x-public-default
     :isMinimalViewFromController="($isMinimalViewFromController ?? null)"
 >
-    <x-post :post="$post" />
+    <div class="">
+        <h1 class="text-4xl font-bold mb-3">{{ $post->title }}</h1>
+
+        <div class="prose">
+            {!! $post->content !!}
+        </div>
+    </div>
+
+    @push('scripts')
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+
+            });
+        </script>
+    @endpush
 </x-public-default>
