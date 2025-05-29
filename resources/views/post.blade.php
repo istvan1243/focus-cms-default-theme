6ff80c6f240ebead10577d035e9e79448110b6ca
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-05-29 18:06:33 +0200

    null

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,27 +4,26 @@
     <div class="">
         <h1 class="text-4xl font-bold mb-3">{{ $post->title }}</h1>
 
-        <p class="mb-4">
-            @if(!empty($category))
-                <p class="mb-4">
-                    <a
-                        class="mr-4 font-semibold text-blue-600 hover:text-blue-400"
-                        href="{{ route('front.category', ['category' => $category->name]) }}"
-                        target="_self"
-                    >{{ $category->title }}</a>
+        <p class="mb-1 text-gray-600 w-full">{{ $post->created_at->format('Y-m-d') }}</p>
 
+        <p class="mb-4 w-full">
+
+            @if(!empty($category))
+                <a
+                    class="mr-4 font-semibold text-blue-600 hover:text-blue-400"
+                    href="{{ route('front.category', ['category' => $category->name]) }}"
+                    target="_self"
+                >{{ $category->title }}</a>
             @endif
 
             @if(!empty($tags))
-
-                    @foreach($tags as $tag)
-                        <a
-                            class="mr-2 font-semibold text-blue-600 hover:text-blue-400"
-                            href="{{ route('front.tag', ['tag' => $tag->name]) }}"
-                            target="_self"
-                        >#{{ $tag->title }}</a>
-                    @endforeach
-
+                @foreach($tags as $tag)
+                    <a
+                        class="mr-2 font-semibold text-blue-600 hover:text-blue-400"
+                        href="{{ route('front.tag', ['tag' => $tag->name]) }}"
+                        target="_self"
+                    >#{{ $tag->title }}</a>
+                @endforeach
             @endif
         </p>
 
