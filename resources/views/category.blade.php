a172f04ce9cbea485d986385bf9484f51876bf43
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-05-30 15:27:27 +0200

    null

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -18,7 +18,7 @@
         </div>
     @endif
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
         @foreach($category->posts as $post)
             <div class="p-2">
                 <p class="text-sm text-gray-400">{{ $category->title }}</p>
--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -18,9 +18,9 @@
         </div>
     @endif
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
         @foreach($tag->posts as $post)
-           <div class="p-2">
+           <div class="px-2">
                 <p class="text-sm text-gray-400">{{ $tag->title }}</p>
                 <p class="mt-1 mb-0 text-black text-2xl font-semibold">
                     <a href="{{ route('post.show', ['slug'=>$post->name]) }}" target="_self">{{ $post->title }}</a>
